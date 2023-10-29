6299db6e385009be03e8080f63156a374ddcaca4
GAMREKELI <nekkimark2@gmail.com>
2023-10-29 15:52:47 +0300

    start project

---
+++ API-Service/src/test/java/ru/gamrekeli/API/Service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gamrekeli.API.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/ru/gamrekeli/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gamrekeli.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

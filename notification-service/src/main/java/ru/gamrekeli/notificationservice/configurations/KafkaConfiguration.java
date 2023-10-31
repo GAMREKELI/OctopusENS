ea99bf00595daa40b3c00d1ca716b7c8a7228a68
GAMREKELI <nekkimark2@gmail.com>
2023-10-31 19:22:22 +0300

    add docker

--- notification-service/src/main/java/ru/gamrekeli/notificationservice/configurations/KafkaConfiguration.java
+++ notification-service/src/main/java/ru/gamrekeli/notificationservice/configurations/KafkaConfiguration.java
@@ -27,20 +27,18 @@ public class KafkaConfiguration {
         return new KafkaTemplate<>(producerFactory);
     }
 
-
-    /**
-     * Настройка consumer для чтения из нескольких партиций одновременно
-     */
     @Bean
     public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
-            DefaultErrorHandler defaultErrorHandler
+            DefaultErrorHandler errorHandler
     ) {
+        // Позволяет создавать консюмеров, которые могут обрабатывать сообщения из нескольких партиций Kafka одновременно,
+        // а также настраивать параметры такие как количество потоков, хэндлинг и т.д.
         ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
-
+        // Настройка фабрики для создания консьюмера Kafka
         kafkaListenerContainerFactory.setConsumerFactory(consumerFactory);
-        kafkaListenerContainerFactory.setCommonErrorHandler(defaultErrorHandler);
-
-        // Указываем количество потоков
+        // Возврат сообщений в DLT очередь
+        kafkaListenerContainerFactory.setCommonErrorHandler(errorHandler);
+        // Обработка сообщений в 4 потока
         kafkaListenerContainerFactory.setConcurrency(4);
         return kafkaListenerContainerFactory;
     }
@@ -49,7 +47,11 @@ public class KafkaConfiguration {
      * Публикатор в dead-letter topic.
      */
     @Bean
-    public DeadLetterPublishingRecoverer publishing(KafkaTemplate<Object, Object> bytesTemplate) {
+    public DeadLetterPublishingRecoverer publisher(KafkaTemplate<Object, Object> bytesTemplate) {
+        //  Определяем логику выбора партиции для отправки сообщения в DLT.
+        //  В данном случае, создаём новый объект TopicPartition, используя имя топика (consumerRecord.topic()) и добавляя суффикс DLT_TOPIC_SUFFIX,
+        //  а также номер партиции (consumerRecord.partition()).
+        //  Следовательно в DLT топике должно быть столько партиций сколько и в топике откуда читаем
         return new DeadLetterPublishingRecoverer(bytesTemplate, (consumerRecord, exception) ->
                 new TopicPartition(consumerRecord.topic() + DLT_TOPIC_SUFFIX, consumerRecord.partition()));
     }
@@ -58,9 +60,9 @@ public class KafkaConfiguration {
      * Обработчик исключений при получении сообщений из kafka по умолчанию.
      */
     @Bean
-    public DefaultErrorHandler defaultErrorHandler(DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
+    public DefaultErrorHandler errorHandler(DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
         final var handler = new DefaultErrorHandler(deadLetterPublishingRecoverer);
-
+        // Обрабатываем любые исключения и отправляем в DLT
         handler.addNotRetryableExceptions(Exception.class);
         return handler;
     }

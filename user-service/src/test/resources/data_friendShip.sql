f63cf4fef63662e174d77f745909116ad45cbf49
pdursley <nekkimark2@gmail.com>
2023-12-30 23:21:14 +0300

    добавлены актуальные зависимости для testcontainers

--- user-service/src/test/resources/data_friendShip.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO friendship (friend_ship_id, user_id, friend_id, status)
-VALUES
-    (1, 1, 2, 'YES'), -- Дружба между пользователем 1 и пользователем 2
-    (2, 1, 3, 'YES'),
-    (3, 2, 3, 'NO');

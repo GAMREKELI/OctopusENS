c9615d636e63dc4fb489597c0359427e4c740081
pdursley <nekkimark2@gmail.com>
2023-12-30 18:34:49 +0300

    доработана логика друзей и подписчиков

--- user-service/src/main/java/ru/gamrekeli/userservice/repository/FriendShipRepository.java
+++ user-service/src/main/java/ru/gamrekeli/userservice/repository/FriendShipRepository.java
@@ -1,5 +1,6 @@
 package ru.gamrekeli.userservice.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,17 +13,27 @@ import java.util.List;
 @Repository
 public interface FriendShipRepository extends JpaRepository<FriendShip, Long> {
 
-    @Query("SELECT f FROM FriendShip f WHERE f.user.userId = :userId AND f.status = NO")
+    @Query("SELECT f FROM FriendShip f WHERE f.friend.userId = :userId AND f.status = NO")
     List<FriendShip> findAllSubscribersByUserId(@Param("userId") Long userId);
 
     @Query("SELECT f FROM FriendShip f WHERE (f.user.userId = :userId OR f.friend.userId = :userId) AND f.status = YES")
     List<FriendShip> findAllFriendsByUserId(@Param("userId") Long userId);
 
     @Modifying
-    @Query("UPDATE FriendShip f SET f.status = YES WHERE f.user.userId = :userId AND f.friend.userId = :friendId")
+    @Transactional
+    @Query("UPDATE FriendShip f SET f.status = YES WHERE f.user.userId = :friendId AND f.friend.userId = :userId")
     int confirmationAddFriend(@Param("userId") Long userId, @Param("friendId") Long friendId);
 
     @Modifying
-    @Query("UPDATE FriendShip f SET f.status = NO WHERE f.user.userId = :userId AND f.user.friendId = :friendId")
+    @Transactional
+    @Query("UPDATE FriendShip f SET f.status = NO WHERE (f.user.userId = :userId AND f.friend.userId = :friendId) OR (f.user.userId = :friendId AND f.friend.userId = :userId)")
     int deleteFriend(@Param("userId") Long userId, @Param("friendId") Long friendId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE FriendShip f SET f.user.userId = :friendId, f.friend.userId = :userId WHERE (f.user.userId = :userId AND f.friend.userId = :friendId) OR (f.user.userId = :friendId AND f.friend.userId = :userId)")
+    void replaceUserIdAndFriendId(@Param("userId") Long userId, @Param("friendId") Long friendId);
+
+    @Query("SELECT COUNT(1) FROM FriendShip f WHERE (f.user.userId = :userId AND f.friend.userId = :friendId) OR (f.user.userId = :friendId AND f.friend.userId = :userId)")
+    int countFriendShipByUserIdAndFriendId(@Param("userId") Long userId, @Param("friendId") Long friendId);
 }

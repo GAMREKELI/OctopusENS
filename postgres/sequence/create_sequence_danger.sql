e541135097ebf25099fdbd74457a0fe3c817aeab
pdursley <nekkimark2@gmail.com>
2024-02-24 14:18:16 +0300

    добавлен sequence

---
+++ postgres/sequence/create_sequence_danger.sql
@@ -0,0 +1,5 @@
+create sequence S_DANGER
+    increment by 1
+    start with 1
+    minvalue 1
+    no cycle;
---
+++ postgres/sequence/create_sequence_users.sql
@@ -0,0 +1,5 @@
+create sequence S_USERS
+    increment by 1
+    start with 1
+    minvalue 1
+    no cycle;
